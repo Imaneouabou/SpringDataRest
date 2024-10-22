c28a76d641bac1993de4e6204ba5c97277e099bd
Imane Ouabou <imane_ouabou@um5.ac.ma>
2024-10-22 19:25:50 +0100

    Ajout de dépendance test + Test class

--- src/test/java/org/cours/SpringDataRestApplicationTests.java
+++ src/test/java/org/cours/SpringDataRestApplicationTests.java
@@ -1,13 +1,21 @@
 package org.cours;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.cours.web.VoitureController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataRestApplicationTests {
 
+    @Autowired
+    private VoitureController voitureController;  // Injection du contrôleur Voiture
+
     @Test
     void contextLoads() {
+        // Vérifier si le contrôleur a été injecté avec succès
+        assertThat(voitureController).isNotNull();
     }
-
 }
